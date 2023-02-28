4e973e17be91cb37f158433ecde6334dc585c728
MarcosSeis <92883516+MarcosSeis@users.noreply.github.com>
2023-02-28 14:09:53 -0600

    Update noticiasProvider.jsx

--- src/context/noticiasProvider.jsx
+++ src/context/noticiasProvider.jsx
@@ -13,7 +13,7 @@ const NoticiasProvider = ({children}) => {
     useEffect(() => {
       const consultarAPI = async () => {
         
-        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=e66f425cb7604f94aff656cc5fd3530a`
 
         const { data } = await axios(url);
 
